ec1d173de80a4a939379bda7d540d2061cc5f25b
illupovero <davideluporini1996@gmail.com>
2022-01-21 16:40:51 +0100

    solved

--- js/snack3.js
+++ js/snack3.js
@@ -4,10 +4,25 @@
 // Usiamo i nuovi metodi degli array visti oggi in classe.
 
 // funzione random
+const numbersBetween = (array, x, y) => {
+    const numBetween = [];
+    for (let i = x; i <= y; i++) {
+        numBetween.push(array[i]);
+    }
+    return numBetween;
+}
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 // Elementi da utilizzare
-const num = [5, 10, 13, 71, 45, 67, 4];
+const array = ['io', 'tu', 'loro', 'acqua', 45, 67, 4];
 const min = getRandomNumber(0, 2);
-const max = getRandomNumber(5, 7);
+const max = getRandomNumber(4, 6);
 
 console.log(`i valori sono compresi fra la posizione ${min} e la posizione ${max}`)
+
+
+// scrivo la funzione
+// esecuzione
+
+// richiamo la funzione
+const numBetween = numbersBetween(array, min, max);
+console.log(numBetween);
